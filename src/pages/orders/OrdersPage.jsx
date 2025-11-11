7f7966e748f50b889b7bd7e8a4350b9aad31532d
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2025-11-11 14:37:09 -0600

    Orders page component

---
+++ src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,24 @@
+import { useFetchOrders } from "../../hooks/useFetchOrders";
+
+export default function OrdersPage() {
+  const { orders, loading, error } = useFetchOrders();
+
+  console.log('orders data')
+  console.log(orders)
+
+  if (loading) return <p>Loading orders...</p>;
+  if (error) return <p>Error loading orders 😢</p>;
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Orders</h2>
+      <ul>
+        {orders.map(order => (
+          <li key={order.id}>
+            <strong>{order.status}</strong> - {order.totalPrice}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
